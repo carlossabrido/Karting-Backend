ad8a0decabedce1c6a098a69bda60eab8497098e
Carlos <carlossabrido12@gmail.com>
2023-06-06 10:27:36 +0200

    create circuit function done

--- entities/circuit/router.ts
+++ entities/circuit/router.ts
@@ -1,8 +1,35 @@
 import express from "express";
+import { createCircuit, deleteCircuit, listCircuit } from "./controller.js";
+import { auth } from "../../core/middleware.js";
 
 const router=express.Router()
 
+router.get('/',async(req,res,next)=>{
+    try{
+        res.json(listCircuit())
+    }
+    catch(e){
+        next(e)
+    }
+})
 
+router.post('/', auth , async(req,res, next)=>{
+        try{
+            res.json(createCircuit(req))
+        }
+        catch(e){
+            next(e)
+        }
+
+})
+
+router.delete("/:id", auth,  async (req, res, next) => {
+    try {
+      res.json(await deleteCircuit(req));
+    } catch (e) {
+      next(e);
+    }
+  });
 
 
 
